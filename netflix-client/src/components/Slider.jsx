833c1e37f39ed3e8a22cb988ca727195d27d44c9
CharlyLuna <cluna10@ucol.mx>
2023-05-07 12:19:54 -0600

    Add condition in case no movies are fetched

--- netflix-client/src/components/Slider.jsx
+++ netflix-client/src/components/Slider.jsx
@@ -6,13 +6,25 @@ export const Slider = ({ movies }) => {
   }
 
   return (
-    <div className='bg-zinc-900 pb-16'>
-      <CardSlider title='Trending now' data={getMoviesFromRange(0, 10)} />
-      <CardSlider title='New Releases' data={getMoviesFromRange(10, 20)} />
-      <CardSlider title='Blockbuster Movies' data={getMoviesFromRange(20, 30)} />
-      <CardSlider title='Popular On Netflix' data={getMoviesFromRange(30, 40)} />
-      <CardSlider title='Action movies' data={getMoviesFromRange(40, 50)} />
-      <CardSlider title='Epics' data={getMoviesFromRange(50, 60)} />
-    </div>
+    <>
+      {
+        movies.length > 0
+          ? (
+            <div className='bg-zinc-900 pb-16'>
+              <CardSlider title='Trending now' data={getMoviesFromRange(0, 10)} />
+              <CardSlider title='New Releases' data={getMoviesFromRange(10, 20)} />
+              <CardSlider title='Blockbuster Movies' data={getMoviesFromRange(20, 30)} />
+              <CardSlider title='Popular On Netflix' data={getMoviesFromRange(30, 40)} />
+              <CardSlider title='Action movies' data={getMoviesFromRange(40, 50)} />
+              <CardSlider title='Epics' data={getMoviesFromRange(50, 60)} />
+            </div>
+            )
+          : (
+            <div className='h-full p-12 text-2xl text-center'>
+              No se encontraron peliculas...
+            </div>
+            )
+      }
+    </>
   )
 }
